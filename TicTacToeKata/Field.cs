f3ced4d037b9179607bd624e42d1be07bbc92d7b
OnlyOneGabe <gabriel.j.ortiz@gmail.com>
2017-06-08 21:32:48 -0700

    Moved Field class to own file

---
+++ TicTacToeKata/Field.cs
@@ -0,0 +1,9 @@
+﻿namespace TicTacToeKata
+{
+    public class Field
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public Player TakenBy { get; set; }
+    }
+}
--- TicTacToeKata/TicTacToeGame.cs
+++ TicTacToeKata/TicTacToeGame.cs
@@ -43,11 +43,4 @@ namespace TicTacToeKata
             }
         }
     }
-
-    public class Field
-    {
-        public int Row { get; set; }
-        public int Column { get; set; }
-        public Player TakenBy { get; set; }
-    }
 }
